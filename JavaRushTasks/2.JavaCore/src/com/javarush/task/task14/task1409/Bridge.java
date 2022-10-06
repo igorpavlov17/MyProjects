6e9fcb0e1e7286ea2b6ab650fc86055c9233b767
igorpavlov17 <iterx73@yandex.ru>
2022-10-06 10:47:02 +0300

    delete

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task14/task1409/Bridge.java
+++
@@ -1,5 +0,0 @@
-package com.javarush.task.task14.task1409;
-
-public interface Bridge {
-    public int getCarsCount();
-}
--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task25/task2508/CustomThreadManipulator.java
+++
@@ -1,7 +0,0 @@
-package com.javarush.task.task25.task2508;
-
-public interface CustomThreadManipulator {
-    public void start(String threadName);
-
-    public void stop();
-}
